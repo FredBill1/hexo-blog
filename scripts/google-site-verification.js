3be71c6ff2d3b49066cd5115e9b69b5fae3961e3
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2024-01-02 19:16:14 +0000

    remove google site verification

--- scripts/google-site-verification.js
+++
@@ -1,3 +0,0 @@
-hexo.extend.filter.register('theme_inject', function (injects) {
-    injects.head.raw('google-site-verification', '<meta name="google-site-verification" content="5tyL-1P6pgVjMW6Wwyx06vNCixQTsvEdzGR_UtfQ4co" />');
-});
